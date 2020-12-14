61e0e501c3d1ced41f3404f5461112c8f0c81cd9
HoussamMrabet <dodslim100@gmail.com>
2020-12-14 01:58:11 +0100

    generate colors

---
+++ app.js
@@ -0,0 +1,39 @@
+//Selections
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const slider = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+let initialColors;
+
+//Functions
+
+    //Color Generator
+
+        /* function generateHex() {
+            const letters = '0123456789ABCDEF';
+            let hash = '#';
+            for (let i = 0; i < 6; i++){
+                hash += letters[Math.floor(Math.random() * 16)];
+            }
+            return hash;
+        } */
+
+        function generateHex() {
+            const hexColor = chroma.random();
+            return hexColor;
+        }
+
+        function randomColors() {
+            colorDivs.forEach((div, index) => {
+                const hexText = div.children[0];
+                const randomColor = generateHex();
+
+                //Add the color to the background
+                div.style.backgroundColor = randomColor;
+                hexText.innerText = randomColor;
+
+            });
+        } 
+        
+        randomColors();
+    
