9685ac4a6e54e59e800d214dd945bfe3f5450da4
HoussamMrabet <dodslim100@gmail.com>
2020-12-14 20:18:07 +0100

    initialize (hue , saturation , brightness) range values while refreshing

--- app.js
+++ app.js
@@ -59,6 +59,9 @@
                 colorizeSliders(color, hue, brightness, saturation);
 
             });
+
+            //reset Inputs
+            resetInputs();
         } 
         
         function checkTextContrast(color, text) {
@@ -122,6 +125,27 @@
                 checkTextContrast(color, icon);
             }
 
+        }
+        
+        function resetInputs() {
+            const sliders = document.querySelectorAll('.sliders input');
+            sliders.forEach(slider => {
+                if (slider.name === 'hue') {
+                    const hueColor = initialColors[slider.getAttribute('data-hue')];
+                    const hueValue = chroma(hueColor).hsl()[0];
+                    slider.value = Math.floor(hueValue);
+                }
+                if (slider.name === 'brightness') {
+                    const brightColor = initialColors[slider.getAttribute('data-bright')];
+                    const brightValue = chroma(brightColor).hsl()[2];
+                    slider.value = Math.floor(brightValue * 100) / 100;
+                }
+                if (slider.name === 'saturation') {
+                    const satColor = initialColors[slider.getAttribute('data-sat')];
+                    const satValue = chroma(satColor).hsl()[1];
+                    slider.value = Math.floor(satValue * 100) / 100;
+                }
+            });
         }
 
         randomColors();
