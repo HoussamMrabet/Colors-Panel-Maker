b72863f5e52fe239cbdded934d8916adaff98eb8
HoussamMrabet <dodslim100@gmail.com>
2020-12-14 16:22:49 +0100

    styling the colors range settings

--- app.js
+++ app.js
@@ -35,6 +35,15 @@ let initialColors;
                 //Contrast Check
                 checkTextContrast(randomColor, hexText);
 
+                //Initial Colorize Sliders
+                const color = chroma(randomColor);
+                const sliders = div.querySelectorAll('.sliders input');
+                const hue = sliders[0];
+                const brightness = sliders[1];
+                const saturation = sliders[2];
+
+                colorizeSliders(color, hue, brightness, saturation);
+
             });
         } 
         
@@ -45,6 +54,23 @@ let initialColors;
             } else {
                 text.style.color = 'white';
             }
+        }
+        
+        function colorizeSliders(color, hue, brightness, saturation) {
+            //scale Saturation
+            const noSat = color.set('hsl.s', 0);
+            const fullSat = color.set('hsl.s', 1);
+            const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+            //scale Brightness
+            const midBright = color.set('hsl.l', 0.5);
+            const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+            //Update Input Colors
+            saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(0)},${scaleSat(1)})`;
+            brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(0)},${scaleBright(0.5)},${scaleBright(1)})`;
+            hue.style.backgroundImage = `linear-gradient(to right,rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+
         }
         
         
